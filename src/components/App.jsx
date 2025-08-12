053a88215ce72d2a87a9aafa8107ae314832614b
Zakhar Veremchuk <zaharverem@gmail.com>
2025-08-12 22:37:23 +0300

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,8 @@ import { Alert1 } from './Alert1'
 import { Alert2 } from './Alert2'
 import { Alert3 } from './Alert3'
 import { HiUser } from "react-icons/hi";
+import  ClickSaveHook  from './ClickSaveHook' 
+import { useState } from 'react';
 
 const favBooks = [
     {id: "id-1", name: "JS for beginners"},
@@ -15,6 +17,54 @@ const favBooks = [
 ]
 
 export default function App() {
+  const [clicks, setClicks] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+
+  const [values, setValues] = useState({
+      x:0,
+      y:0,
+  });
+
+  const updateX = () => {
+    setValues({
+      ...values,
+      x: values.x + 1
+    });
+  };
+  const updateY = () => {
+    setValues({
+      ...values,
+      y: values.y + 1
+    });
+  };
+
+  // example that you can add as much as you want
+  // сonst [a, setA] = useState();
+  // const [b, setB] = useState();
+  // const [c, setC] = useState();
+  const handleClick = () => {
+    alert("Im a button!");
+  };
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const doSmth = (evt) => {
+    console.log(evt);
+  };
+
+  const countClicks = () => {
+    setClicks(clicks + 1);
+  };
+
+  const CustomButton = ({message, children}) => {
+    return (
+        <button onClick={() => alert(message)}>{children}</button>
+    )
+  }
+
   return (
     <>
       <h1>Best selling</h1>
@@ -66,6 +116,28 @@ export default function App() {
       <br />
       <div>
         <p><HiUser/></p>
+      </div>
+
+      <button onClick={handleClick}> Click me!</button>
+      <button onClick={() => alert("Im a button!")}>Click me too</button>
+      <button onClick={doSmth}>Obj of event of browser(SyntheticBaseEvent)</button>
+      <CustomButton message="Playing music">Play some music!</CustomButton>
+      <CustomButton message="Uploading your data!">Upload data</CustomButton>
+      <button onClick={countClicks}>Current: {clicks}</button>
+      <button onClick={handleToggle}>{isOpen ? "Hide": "Show"}</button>
+      {isOpen && <p>Now you can see me!</p>}
+
+      <div>
+        <p>
+          x: {values.x} , y: {values.y}
+        </p>
+        <button onClick={updateX}>Update x</button>
+        <button onClick={updateY}>Update y</button>
+      </div>
+
+      <div>
+        <ClickSaveHook/>
+        <Experement/>
       </div>
     </>
   );
---
+++ src/components/ClickSaveHook.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+const ClickSaveHook = () => {
+    const [clicks, setClicks] = useState(0);
+
+    // init effect 
+    useEffect(() => {
+        document.title = `You clicked ${clicks} times`;
+    })
+
+    return (
+        <button onClick={() => setClicks(clicks + 1)}>
+            You clicked {clicks} times
+        </button>
+    )
+
+}
+
+export default ClickSaveHook;
